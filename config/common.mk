da66c2d2ce5484eb4997aebe2816e41a77718781
Michael Bestas <mkbestas@lineageos.org>
2023-10-09 16:10:05 +0300

    RIP Android Beam
    Change-Id: Ia977097d40b7f0538f2b15d231e69f2fa22adc4c

--- config/common.mk
+++ config/common.mk
@@ -91,10 +91,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
-# Enable Android Beam on all targets
-PRODUCT_COPY_FILES += \
-    vendor/xdroid/config/permissions/android.software.nfc.beam.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.nfc.beam.xml
-
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.sip.voip.xml
