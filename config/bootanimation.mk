d7be79e2e4daafabd929287540daca74a0fc9da2
Julius Ronald Christanto <juliusronald12@gmail.com>
2023-12-05 03:28:07 +0700

    vendor: Rebrand xdroidOSS

---
+++ config/bootanimation.mk
@@ -0,0 +1,22 @@
+# xd. Bootanimation
+ifeq ($(XDROID_BOOT),)
+$(warning "xdroidUI: Using xd. Dark Bootanimation legacy")
+    PRODUCT_COPY_FILES += vendor/xdroid/overlay/bootanimation/xd_boot_legacy.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else
+ifeq ($(XDROID_BOOT),480)
+$(warning "xdroidUI: Using xd. Dark Bootanimation 480p")
+    PRODUCT_COPY_FILES += vendor/xdroid/overlay/bootanimation/xd_boot_480.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+endif
+ifeq ($(XDROID_BOOT),720)
+$(warning "xdroidUI: Using xd. Dark Bootanimation 720p")
+    PRODUCT_COPY_FILES += vendor/xdroid/overlay/bootanimation/xd_boot_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+endif
+ifeq ($(XDROID_BOOT),1080)
+$(warning "xdroidUI: Using xd. Dark Bootanimation 1080p")
+    PRODUCT_COPY_FILES += vendor/xdroid/overlay/bootanimation/xd_boot_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+endif
+ifeq ($(XDROID_BOOT),1440)
+$(warning "xdroidUI: Using xd. Dark Bootanimation 1440p")
+    PRODUCT_COPY_FILES += vendor/xdroid/overlay/bootanimation/xd_boot_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+endif
+endif
--- config/telephony.mk
+++ config/telephony.mk
@@ -5,7 +5,7 @@ endif
 
 # Sensitive Phone Numbers list
 PRODUCT_PACKAGES += \
-    sensitive_pn.xml
+    vendor/xdroid/prebuilt/common/etc/sensitive_pn.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sensitive_pn.xml
 
 # World APN list
 PRODUCT_PACKAGES += \
@@ -15,10 +15,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     messaging \
     Stk
-
-# Default ringtone
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.config.ringtone=Orion.ogg
 
 # Tethering - allow without requiring a provisioning app
 # (for devices that check this)
