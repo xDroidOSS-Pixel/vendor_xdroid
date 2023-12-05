ae113ecf949f988b7e3e537a062982ea9ab2a64b
Julius Ronald Christanto <juliusronald12@gmail.com>
2023-12-05 12:52:24 +0700

    vendor : Initial rebrand for xdroidOSS

--- build/core/main.mk
+++ build/core/main.mk
@@ -1,2 +1,2 @@
-# Include LineageOS versions
-include $(TOPDIR)vendor/lineage/build/core/main_version.mk
+# Include xdroidOSS versions
+include $(TOPDIR)vendor/xdroid/build/core/main_version.mk
--- config/common_full.mk
+++ config/common_full.mk
@@ -1,24 +1,7 @@
-# Inherit common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_mobile.mk)
+# Inherit common stuff
+$(call inherit-product, vendor/xdroid/config/common_mobile.mk)
 
 PRODUCT_SIZE := full
-
-# Include {Lato,Rubik} fonts
-$(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
-$(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
-
-# Apps
-PRODUCT_PACKAGES += \
-    Eleven \
-    Etar \
-    Profiles \
-    Recorder \
-    Seedvault
-
-ifneq ($(PRODUCT_NO_CAMERA),true)
-PRODUCT_PACKAGES += \
-    Aperture
-endif
 
 ifneq ($(TARGET_EXCLUDES_AUDIOFX),true)
 PRODUCT_PACKAGES += \
@@ -29,9 +12,3 @@ endif
 PRODUCT_PACKAGES += \
     unrar \
     zstd
-
-# Fonts
-PRODUCT_PACKAGES += \
-    fonts_customization.xml \
-    FontLatoOverlay \
-    FontRubikOverlay
